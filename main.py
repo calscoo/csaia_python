7707fd5014990c51f5df9d48bdc9aa02f0ee6bd3
calebols <caleb.s.olson@ndsu.edu>
2021-02-08 23:58:58 -0600

    Updated to support TIFF files.

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
 import os
-from PIL import Image, ExifTags
+import exifread
+from GPSPhoto import gpsphoto
 
-for root, subdirs, files in os.walk('C:\\Users\\Caleb\\Downloads\\CCAST_CSAIA2'):
+for root, subdirs, files in os.walk('C:\\Users\\Caleb\\Downloads'):
     oneImage = False
     for file in files:
-        if os.path.splitext(file)[1].lower() in ('.jpg', '.jpeg'):
+        ext = os.path.splitext(file)[1].lower()
+        if ext in ('.jpg', '.jpeg', '.tif', '.tiff'):
             # This flag stops the iteration on the first image, uncomment this to iterate over the entire directory provided
             oneImage = True
             imgPath = os.path.join(root, file)
-            img = Image.open(imgPath)
-            exif = {ExifTags.TAGS[k]: v for k, v in img._getexif().items() if k in ExifTags.TAGS}
-            print(imgPath)
-            for mdChunk in exif:
-                print(mdChunk, ':', exif[mdChunk])
+            f = open(imgPath, 'rb')
+            tags = exifread.process_file(f)
+            gpsData = gpsphoto.getGPSData(imgPath)
+            print(gpsData)
+            for tag in tags.keys():
+                print(tag, ":", tags[tag])
         if oneImage:
             break
     if oneImage:
