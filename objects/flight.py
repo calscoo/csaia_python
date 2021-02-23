ca0b7516757a5804f1a19fecc4f58f7c6d5ab1a9
calscoo <scoutscott13@gmail.com>
2021-02-22 20:20:15 -0600

    Added the image curator for the CCAST system, created preliminary previous_flight_curator and flight_manager files. Created flight and image objects

---
+++ objects/flight.py
@@ -0,0 +1,16 @@
+class image:
+    def __init__(self, id, user_id, flight_name, manual_notes, address, field_name, crop_name, average_latitude, average_longitude, average_altitude, flight_start_time, flight_end_time, hardware_make, hardware_model):
+        self.id = id
+        self.user_id = user_id
+        self.flight_name = flight_name
+        self.manual_notes = manual_notes
+        self.address = address
+        self.field_name = field_name
+        self.crop_name = crop_name
+        self.average_latitude = average_latitude
+        self.average_longitude = average_longitude
+        self.average_altitude = average_altitude
+        self.flight_start_time = flight_start_time
+        self.flight_end_time = flight_end_time
+        self.hardware_make = hardware_make
+        self.hardware_model = hardware_model
