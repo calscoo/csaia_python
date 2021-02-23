996d4a2ace67480ed306d1c2be2156177a8b5b4c
Ben Harvey <benjamin.c.harvey@ndsu.edu>
2021-02-23 12:13:22 -0600

    added flask server directory

---
+++ server/server.py
@@ -0,0 +1,35 @@
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+from objects import flight
+import os
+
+app = Flask(__name__)
+
+# CORS policies need to be modified, this isn't secure
+cors = CORS(app, resources={r'/*': {"origins": '*'}})
+app.config['CORS_HEADERS'] = 'Content-Type'
+
+# these need to be changed before deployment
+app.config['ENV'] = 'development'
+app.config['DEBUG'] = True
+app.config['TESTING'] = True
+
+# Flight Upload Endpoint
+@app.route('/upload', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+
+        flight = flight()
+
+        # request.files contains all the files attached to the request
+        for file in request.files.getlist('image'):
+            file.save(os.path.join('uploaded', file.filename))
+
+        
+        return jsonify(success=True)
+
+
+
+
+if __name__ == '__main__':
+   app.run()
