457ccdbaf455158682151f0c9a817350789ae224
calscoo <scoutscott13@gmail.com>
2021-02-26 16:29:33 -0600

    Increased the db image length and width from smallint to int, started preliminary work on flight curation

---
+++ adhoc/curate.sql
@@ -0,0 +1,13 @@
+CREATE TABLE `csaia_database`.`curate` (
+  `image_id` INT NOT NULL,
+  `image_parent_location` TEXT NOT NULL
+);
+
+-- INSERT INTO csaia_database.curate(`image_id`, `image_parent_location`) SELECT id, SUBSTRING_INDEX(directory_location, SUBSTRING_INDEX(directory_location, '/' ,-1), 1) AS flight_name FROM csaia_database.images;
+-- UPDATE csaia_database.images SET `flight_id` = (
+SELECT csaia_database.images.id AS image_id, csaia_database.flights.id AS flight_id
+FROM csaia_database.flights
+INNER JOIN csaia_database.curate
+ON csaia_database.flights.flight_name = csaia_database.curate.image_parent_location
+INNER JOIN csaia_database.images
+ON csaia_database.images.id = csaia_database.curate.image_id;
--- main.py
+++ main.py
@@ -6,17 +6,17 @@ image_directory = 'C:\\Users\\Caleb\\Downloads\\CCAST_CSAIA2'
 #image_manager.upload_images(image_directory)
 flight_manager.build_flight(image_directory,'Test', 'Test', 'Test', 'Test')
 
-image_ids = None
-user_ids = None
-flight_ids = None
-extensions = ['.png', '.jpg', '.tif']
-datetime_range = range('2012-01-01 00:00:00', '2022-01-01 00:00:00')
-latitude_range = range(46, 48)
-longitude_range = range(-100, -98)
-altitude_range = range(450, 500)
-make = 'mica'
-model = 'edge'
-
-images = image_manager.fetch_images(image_ids, user_ids, flight_ids, extensions, datetime_range, latitude_range, longitude_range, altitude_range, make, model)
-for image in images:
-    print(str(image))
+# image_ids = None
+# user_ids = None
+# flight_ids = None
+# extensions = ['.png', '.jpg', '.tif']
+# datetime_range = range('2012-01-01 00:00:00', '2022-01-01 00:00:00')
+# latitude_range = range(46, 48)
+# longitude_range = range(-100, -98)
+# altitude_range = range(450, 500)
+# make = 'mica'
+# model = 'edge'
+#
+# images = image_manager.fetch_images(image_ids, user_ids, flight_ids, extensions, datetime_range, latitude_range, longitude_range, altitude_range, make, model)
+# for image in images:
+#     print(str(image))
