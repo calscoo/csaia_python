0fc7502053a07bdde7444c517d5043b36055afb2
calscoo <scoutscott13@gmail.com>
2021-04-27 00:00:25 -0500

    Updated first user password

--- first_user_add.py
+++ first_user_add.py
@@ -7,4 +7,4 @@ from managers import flight_manager, users_manager, image_manager
 # Creates the very first user in CSAIA.
 # This file needs to be executed on the command line, since user creation typically requires an admin and non exist yet.
 # This method is NOT available through the API
-users_manager.create_user_testing("user1", "pass", roles.Admin)
+users_manager.create_user_testing("paulo.flores@ndsu.edu", "csaiapassword", roles.Admin)
